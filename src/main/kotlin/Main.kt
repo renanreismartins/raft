5f88b0e220ad967f07c8e89332e00c035b82c1a7
Aidan Dagnall <aidan.dagnall@transferwise.com>
2024-03-20 09:42:04 +0000

    Session 2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -10,6 +10,24 @@ data class Message(val dest: InetAddress, val src: InetAddress)
 
 data class Address(val host: String, val port: Int)
 
-data class Node(val address: Address, val name: String) {
+interface Node {
+   fun tick() : Pair<Node,List<Message>>;
+   fun receive(message: Message) : Node;
+}
 
+data class Follower(val address: Address, val name: String) : Node {
+    val queue = Queue()
+
+
+    fun tick() {
+       this.send(RforVotes(n1, n2, n3))
+    }
+
+
+}
+
+
+nodes.forEach { n ->
+  val (new, message) = n.tick
+  messages.
 }
--- src/test/kotlin/NodeTests.kt
+++ src/test/kotlin/NodeTests.kt
@@ -1,15 +1,24 @@
 import org.example.Address
-import org.example.Node
-import java.net.InetAddress
+import org.example.Follower
 
 class NodeTests {
 
 // one node cluster
-    // message arriving in one-node system
-    fun `On initialisation, node will start as follower`() {
+    fun `Follower stays as a follower if it receives a heartbeat from a leader before the election timeout (3 ticks)`() {
+        Follower(Address("127.0.0.1", 9001), "NodeA")
+        follower.tick().receive(LeaderHeartBeat())
+        follower.consume()
+        follower.tick()
+        follower.tick()
+        follower == Follower
     }
 
-    fun `Node will start an election`() {
+    fun `Follower moves to Candidate after election timeout (3 ticks)`() {
+        val follower = Follower(Address("127.0.0.1", 9001), "NodeA")
+        follower.tick()
+        follower.tick()
+        follower.tick()
+        follower == Candidate
     }
 
     fun `Creates a RequestVote message`() {
