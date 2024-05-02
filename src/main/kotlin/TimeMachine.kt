b04ecca22a6907fedec64e2c3ac966fd7cf664eb
Aidan Dagnall <aidan.dagnall@transferwise.com>
2024-05-02 09:34:42 +0100

    Add clarifying comment to the TimeMachine, we need to make sure the network is ticked first when ticking the system.

--- src/main/kotlin/TimeMachine.kt
+++ src/main/kotlin/TimeMachine.kt
@@ -3,7 +3,10 @@ package org.example
 data class TimeMachine(val network: Network, val nodeA: Node, val nodeB: Node) {
     fun tick(): TimeMachine {
         return this.copy(
-            network = network.tick(), nodeA = nodeA.tick(), nodeB = nodeB.tick()
+            // the Network must come first, as the Nodes rely on its clock when they tick
+            network = network.tick(),
+            nodeA = nodeA.tick(),
+            nodeB = nodeB.tick()
         )
     }
 
