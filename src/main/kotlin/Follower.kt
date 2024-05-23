4058b409774e0f29a60c5098b94d09818c1d5200
Aidan Dagnall <aidan.dagnall@transferwise.com>
2024-05-23 08:57:56 +0100

    Act on a message based on its type; Refactor promotion criteria;

--- src/main/kotlin/Follower.kt
+++ src/main/kotlin/Follower.kt
@@ -11,11 +11,11 @@ data class Follower(
 ): Node(address, name, state, network, peers, messages) {
 
 
-    fun process(message: Message) {
+    fun process(message: Message): Pair<Int, List<Message>> {
         val state = 0
-        when(message) {
+        return when(message) {
             is Heartbeat -> ((state + message.content.toInt()) to emptyList<Message>())
-            is RequestForVotes -> (state to listOf(VoteFromFollower(address, message.src, "Future content here"))) //TODO add the message here instead of calling 'send'
+            is RequestForVotes -> (state to listOf(VoteFromFollower(address, message.src, "VOTE FROM FOLLOWER"))) //TODO add the message here instead of calling 'send'
             is VoteFromFollower -> (state to emptyList<Message>()) //TODO add the message here instead of calling 'send'; this should never happen, decide how to handle
         }
     }
@@ -23,39 +23,29 @@ data class Follower(
     override fun tick(): Node {
         val tickMessages = network.get(this.address)
 
-        val newState = tickMessages.fold(state) { acc, msg ->
-            try {
-                msg.content.toInt() + acc
-            } catch (e: NumberFormatException) {
-                println("RECEIVED $msg")
-
-                if ("REQUEST FOR VOTES" == msg.content) {
-                    send(VoteFromFollower(address, msg.src, "VOTE FROM FOLLOWER")) //TODO REMOVE THIS SIDE EFFECT
-                }
-
-                acc
-            }
+        val (newState, messagesToSend) = tickMessages.fold(state to emptyList<Message>()) { acc, msg ->
+            process(msg)
         }
+
+        messagesToSend.forEach { send(it) } // TODO remove side effect
 
         val messageLog = messages + tickMessages.map { network.clock to it }
 
-        if (messageLog.isEmpty() && network.clock > config.electionTimeout) {
+        if (shouldPromote(messageLog)) {
             val candidate = Candidate(address, name, state, network, peers, messageLog)
-
             peers.forEach { peer -> candidate.send(RequestForVotes(this.address, peer, "REQUEST FOR VOTES")) } // TODO Refactor to return the messages to be sent instead of a side effect
-
             return candidate
         }
 
-        //TODO In this scenario we are promoting a Follower to Candidate but not sending a Request For Votes, write a test and fix
-        return if (messageLog.isNotEmpty() && network.clock - messageLog.last().first > config.electionTimeout) Candidate(
-            address,
-            name,
-            state,
-            network,
-            peers,
-            messageLog
-        ) else return this.copy(state = newState, messages = messageLog)
+         return this.copy(state = newState, messages = messageLog)
+    }
+
+    private fun shouldPromote(messageLog: List<MessageLogEntry>): Boolean {
+        // On system startup, Follower hasn't received any messages and should promote itself after electionTimeout
+        val hasReachedFirstTimeoutAfterStartup = messageLog.isEmpty() && network.clock > config.electionTimeout
+        // During normal operation of the system, has not received messages in electionTimeout period
+        val hasReachedTimeoutWithLastMessage = messageLog.isNotEmpty() && network.clock - messageLog.last().first > config.electionTimeout
+        return hasReachedTimeoutWithLastMessage || hasReachedFirstTimeoutAfterStartup
     }
 
     override fun receive(message: Message): Node {
