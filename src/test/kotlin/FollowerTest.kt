1b9ea51db51c90de403faa454e796215ef3a394f
Renan Reis Martins de Paula <renanreismartins@gmail.com>
2024-06-19 09:49:20 +0200

    Fix the test that assures a Follower votes for only one Candidate
    The reason for the failure is that we forgot to tick
    the Network and the Node.
    
    We should try to enforce the use of the TimeMachine as it
    tick for all the involved parties.

--- src/test/kotlin/FollowerTest.kt
+++ src/test/kotlin/FollowerTest.kt
@@ -3,6 +3,7 @@ import org.example.Follower
 import org.example.Network
 import org.example.NetworkMessage
 import org.example.RequestForVotes
+import org.example.TimeMachine
 import org.example.VoteFromFollower
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
@@ -14,11 +15,12 @@ class FollowerTest {
             NetworkMessage(RequestForVotes(Address("host", 2), Address("host", 1), "REQUEST FOR VOTES"), 0),
             NetworkMessage(RequestForVotes(Address("host", 3), Address("host", 1), "REQUEST FOR VOTES"), 0)
         )))
-        val follower = Follower(Address("host", 1), "follower", 0, network, emptyList(), emptyList())
 
-        follower.tick()
+        //TODO Write an ADR on why use time machine instead of tick on the Node and Network and why the Network is mutable
+        val timeMachine = TimeMachine(network, Follower(Address("host", 1), "follower", 0, network, emptyList(), emptyList()))
+        val (_, follower) = timeMachine.tick()
 
         //TODO COULD BE THAT THE ADDRESSES ARE WRONG
-        assertEquals(listOf(VoteFromFollower(Address("host", 1), Address("host", 2), "VOTE FROM FOLLOWER")), follower.sent)
+        assertEquals(listOf((1 to VoteFromFollower(Address("host", 1), Address("host", 2), "VOTE FROM FOLLOWER"))), follower.sent)
     }
 }
