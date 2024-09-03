6fb5d23e35d6cea5c295f912f5081ccffab8198d
Renan Reis Martins de Paula <renanreismartins@gmail.com>
2024-09-03 10:27:30 +0200

    Encapsulate the log collection in its own class
    The Log is not a simple collection as its index start at 1.
    This might create some complications on how to calculate its index and
    spread magic numbers all over the code. It is better if we encapsulate it.

---
+++ src/main/kotlin/Log.kt
@@ -0,0 +1,13 @@
+package org.example
+
+class Log(
+    val messages: List<Message> = emptyList(),
+    val index: Int = 1
+) {
+    fun add(message: Message): Log {
+        return Log(
+            messages = this.messages + message,
+            index = this.index + 1
+        )
+    }
+}
