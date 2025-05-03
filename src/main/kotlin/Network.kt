996e54e919e49ae3a2aa86715d9ad23fe9236099
Renan Reis Martins de Paula <renanreismartins@gmail.com>
2025-05-03 19:08:49 -0400

    Re-write explanation on Network class; Identation

--- src/main/kotlin/Network.kt
+++ src/main/kotlin/Network.kt
@@ -15,9 +15,13 @@ class Network(
 
     // TODO Write an ADR
     // The keys are a Pair because we want to use the Host and Port to get the messages. This is because
-    // Address now has specialised types (Source and Destination) and even tho they have the same values
-    // they are compared differently so the messages were not returned
-    // This is to not break the contract of hashcode and equals
+    // Address has specialised types (Source and Destination) and even though they have the same values
+    // when trying to get the messages for a host, the messages might not be returned because of the the
+    // comparison of the Address subtypes. Using this approach we do not need to break the equals and hashcode contracts.
+    // For example, we want to get messages from a host, sometimes we will refer to it as a Source and sometimes as a
+    // Destination, depending where we are in the code, we would like to not have that distinction when getting the
+    // messages.
+    // get(Source("host", port)) and get(Destination("host", port))
     private val messages = initialMessages.mapKeys { it.key.host to it.key.port }.toMutableMap()
 
     fun get(address: Address): List<Message> {
@@ -25,13 +29,11 @@ class Network(
         // get all messages for address with 0 delay and store
         val (messagesToDeliver, remaining) =
             messages[hostAndPort]
-                ?.partition { (_, deliveryTime) ->
-                    deliveryTime <= clock
-                } ?: (listOf<NetworkMessage>() to listOf())
+                ?.partition { (_, deliveryTime) -> deliveryTime <= clock }
+                ?: (listOf<NetworkMessage>() to listOf())
 
         messages[hostAndPort] = remaining
 
-        // return store messages
         return messagesToDeliver.map { it.message }
     }
 
